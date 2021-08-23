fb96daabfd92610e902bee7ca621c805aa47c9a8
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-08-23 13:56:33 +0200

    up

--- src/learningjava/NicknameMap.java
+++ src/learningjava/NicknameMap.java
@@ -5,14 +5,20 @@ import java.util.HashMap;
 public class NicknameMap {
     public static void main(String[] args) {
         HashMap<String, String> nicknameMap = new HashMap<>();
-        nicknameMap.put("Anja", "Anoushka");
+        nicknameMap.put("Ted", "Teddy");
         nicknameMap.put("Lana", "Laney");
         nicknameMap.put("Thomas", "Tommy");
         nicknameMap.put("Barbara", "Barbie");
 
         System.out.println(nicknameMap.get("Lana"));
         System.out.println(nicknameMap.get("Thomas"));
-        System.out.println(nicknameMap.get("Anja"));
+        System.out.println(nicknameMap.get("Ted"));
+
+        nicknameMap.forEach((key,value) -> System.out.println(key + " = " + value));
+
+        nicknameMap.remove("Ted");
+        System.out.println("Ted is removed");
+        nicknameMap.forEach((key,value) -> System.out.println(key + " = " + value));
 
     }
     
---
+++ src/learningjava/Palindrome.java
@@ -0,0 +1,45 @@
+package learningjava;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String ask = "Enter word to check for palindrome:";
+        String input;
+
+
+            do {
+                System.out.println(ask);
+                input = scanner.nextLine();
+
+                if (isPalindrome(input))
+                    System.out.print("Yes, " + input.toUpperCase() + " is a palindrome!");
+                else
+                    System.out.print(input.toUpperCase() + " is not a palindrome.");
+            } while(!isPalindrome(input));
+
+    }
+
+    static boolean isPalindrome(String input){
+ 
+        // Pointers pointing to the beginning and the end of the string
+        int i = 0, j = input.length() - 1;
+ 
+        // While there are characters to compare
+        while (i < j) {
+ 
+            // If there is a mismatch / letters are not same
+            if (input.charAt(i) != input.charAt(j))
+                return false;
+ 
+            // Increment first pointer and decrement the other
+            i++;
+            j--;
+        }
+ 
+        // Given string is a palindrome
+        return true;
+    }
+
+}
