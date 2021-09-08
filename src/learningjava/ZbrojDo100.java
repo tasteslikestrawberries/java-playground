a5ad994295eebd73144c87f5bc1bcdc2c29c64f8
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-09-08 17:42:56 +0200

    up

--- src/learningjava/ZbrojDo100.java
+++ src/learningjava/ZbrojDo100.java
@@ -1,4 +1,31 @@
+//Unositi brojeve sve dok njihov zbroj ne doðe do 100.
+//Nakon toga napraviti ispis zbroja.
+
 package learningjava;
 
+import java.util.Scanner;
+
 public class ZbrojDo100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int i= 0;
+        System.out.println("Unesi brojeve:");
+
+        do {
+            int input = scanner.nextInt();
+            sum += input;
+
+            if(sum>100) {
+                System.out.println("Zbroj je veći od 100.");
+                continue;
+            }
+
+            i++;
+
+        } while (sum<100);
+
+
+    }
 }
